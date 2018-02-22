48f9938a9365734803c1abe1f34e56cc6d522e4f
bartoszgoc <bartoszgoc@gmail.com>
2018-02-22 19:54:01 +0100

    FrogRiverOne

--- Codility_4.1/Codility_4.1/Program.cs
+++ Codility_4.1/Codility_4.1/Program.cs
@@ -11,5 +11,39 @@ namespace Codility_4._1
         static void Main(string[] args)
         {
         }
+
+        class Solution
+        {
+            public int solution(int[] A)
+            {
+                Array.Sort(A);
+                int score = 1;
+                if (A.Length == 1 && A[0] != 1)
+                {
+                    score = 0;
+                }
+                else
+                {
+                    if (A.Length >= 1 && A[0] != 1)
+                    {
+                        score = 0;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < A.Length - 1; i++)
+                        {
+                            if (A[i + 1] - A[i] != 1)
+                            {
+                                score = 0;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return score;
+            }
+
+        }
     }
 }
---
+++ Codility_4.2/Codility_4.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 5;
+            int[] arr = { 1, 3, 1, 4, 2, 3, 5, 4 };
+            Solution sol = new Solution();
+            Console.WriteLine(sol.solution(k, arr));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int X, int[] A)
+        {
+            bool[] leavesInPosition = new bool[X + 1];
+            int taken = 0;
+            int pos = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                pos = A[i];
+                if (pos <= X && !leavesInPosition[pos])
+                {
+                    leavesInPosition[pos] = true;
+                    taken++;
+                }
+
+                if (taken == X)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+
+
+}
+
+
