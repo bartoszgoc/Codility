3e8fc4b202829827ea689a94447791b104285043
bartoszgoc <bartoszgoc@gmail.com>
2018-02-22 18:27:37 +0100

    TapeEquilibrium v1.1

--- codility_3.1/codility_3.1/Program.cs
+++ codility_3.1/codility_3.1/Program.cs
@@ -12,4 +12,29 @@ namespace codility_3._1
         {
         }
     }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            if (A.Length == 2)
+            {
+                return A[1] - A[0];
+            }
+            else
+            {
+                List<int> diff = new List<int>();
+
+                int left = 0;
+                int sum = A.Sum();
+                foreach (var item in A)
+                {
+                    left += item;
+                    diff.Add(Math.Abs((sum - left) - left));
+                }
+                return diff.Min();
+            }
+        }
+    }
+
 }
