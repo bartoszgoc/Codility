f8d1f4e8ba65f5b3f892f8f393024d2b5dd7e49a
bartoszgoc <bartoszgoc@gmail.com>
2018-02-22 17:01:23 +0100

    BinaryGap_v1.1

--- Codility_1/Codility_1/Program.cs
+++ Codility_1/Codility_1/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Codility_1
 {
@@ -10,6 +9,59 @@ namespace Codility_1
     {
         static void Main(string[] args)
         {
+            Solution solution = new Solution();
+            solution.solution(42);
+            Console.ReadKey();
         }
     }
+
+    class Solution
+    {
+        public int solution(int N)
+        {
+            char[] znaki = Convert.ToString(N, 2).ToCharArray();
+            int firstOfOnes = 0;
+            int lastOfOnes = 0;
+
+            for (int i = 0; i < znaki.Length; i++)
+            {
+                if (znaki[i] == '1')
+                {
+                    firstOfOnes = i;
+                    break;
+                }
+            }
+            for (int i = znaki.Length - 1; i >= 0; i--)
+            {
+                if (znaki[i] == '1')
+                {
+                    lastOfOnes = i;
+                    break;
+                }
+            }
+
+            int score = 0;
+            int flaga = 0;
+
+            for (int i = firstOfOnes; i <= lastOfOnes; i++)
+            {
+                if (znaki[i] == '0')
+                {
+                    score++;
+                }
+                else
+                {
+                    if (score > flaga)
+                    {
+                        flaga = score;
+                    }
+                    score = 0;
+                }
+            }
+            return flaga;
+        }
+
+    }
+
+
 }
