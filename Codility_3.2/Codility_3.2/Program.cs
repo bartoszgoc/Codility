344ec7c726473d0decfc5ab3e2a442efbf87d573
bartoszgoc <bartoszgoc@gmail.com>
2018-02-22 18:44:32 +0100

    FrogJmp

---
+++ Codility_3.2/Codility_3.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            
+            Console.WriteLine(sol.solution(20,50,20));
+            Console.ReadKey();
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int X, int Y, int D)
+        {
+            double diff = Y - X;
+
+            int score = Convert.ToInt32(Math.Ceiling(diff / D));
+
+            return score;
+        }
+    }
+}
