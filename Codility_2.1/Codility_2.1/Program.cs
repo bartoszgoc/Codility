14948586ecaa1821ec369eeaa6c46b5b9f73b74e
bartoszgoc <bartoszgoc@gmail.com>
2018-02-22 17:29:57 +0100

    OddOccurrencesInArray

---
+++ Codility_2.1/Codility_2.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1 };
+            Solution sol = new Solution();
+            Console.WriteLine(sol.solution(arr));
+            Console.ReadKey();
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int score = 0;
+            Array.Sort(A);
+
+            if (A.Length != 1)
+            {
+                for (int i = 0; i < A.Length - 1; i = i + 2)
+                {
+                    if (A[i] != A[i + 1])
+                    {
+                        score = A[i];
+                        break;
+                    }
+                    score = A[A.Length - 1];
+                }
+            }
+            else
+            {
+                score = A[0];
+            }
+            return score;
+        }
+    }
+}
